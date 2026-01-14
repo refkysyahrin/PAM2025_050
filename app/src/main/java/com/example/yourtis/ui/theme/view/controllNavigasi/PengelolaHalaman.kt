589c339ee559bb13b739b297090ee707c1b02740
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 18:18:42 +0700

    inisialisasi route navigasi: definisi object destinasi untuk login, register, dan dashboard (petani/pembeli)

--- app/src/main/java/com/example/yourtis/ui/theme/view/controllNavigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/yourtis/ui/theme/view/controllNavigasi/PengelolaHalaman.kt
@@ -1,2 +1,18 @@
 package com.example.yourtis.ui.theme.view.controllNavigasi
 
+// 1. Definisi Route (Alamat Halaman)
+object DestinasiLogin {
+    const val route = "login"
+}
+
+object DestinasiRegister {
+    const val route = "register"
+}
+
+object DestinasiHomePetani {
+    const val route = "home_petani"
+}
+
+object DestinasiHomePembeli {
+    const val route = "home_pembeli"
+}
