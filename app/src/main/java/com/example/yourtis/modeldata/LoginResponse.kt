52bd431fb8ec14a04587a944bbe437b78d2a7d43
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 20:22:25 +0700

    buat model data LoginResponse: data class serializable untuk parsing hasil response API login (message & user)

--- app/src/main/java/com/example/yourtis/modeldata/LoginResponse.kt
+++ app/src/main/java/com/example/yourtis/modeldata/LoginResponse.kt
@@ -1,4 +1,9 @@
 package com.example.yourtis.modeldata
 
-class LoginResponse {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponse(
+    val message: String,
+    val user: User // Objek User di atas
+)
