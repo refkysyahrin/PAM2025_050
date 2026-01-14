53db5a236d69f248c140b88775efa19bcc9c238e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 18:24:36 +0700

    setup MainActivity: set content view ke PengelolaHalaman sebagai root navigasi dalam YourtisTheme

--- app/src/main/java/com/example/yourtis/MainActivity.kt
+++ app/src/main/java/com/example/yourtis/MainActivity.kt
@@ -6,42 +6,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.yourtis.ui.theme.YourtisTheme
+import com.example.yourtis.ui.theme.view.controllNavigasi.PengelolaHalaman
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             YourtisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                    ) {
+                    PengelolaHalaman()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YourtisTheme {
-        Greeting("Android")
-    }
-}
