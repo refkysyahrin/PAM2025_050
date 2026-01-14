dd14c4747f1670a27dc5cfa674b8689409d71460
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-14 20:25:24 +0700

    buat model data Sayur: data class serializable untuk representasi produk dengan atribut id, stok, harga, dan url gambar

--- app/src/main/java/com/example/yourtis/modeldata/Sayur.kt
+++ app/src/main/java/com/example/yourtis/modeldata/Sayur.kt
@@ -1,4 +1,15 @@
 package com.example.yourtis.modeldata
 
-class Sayur {
-}
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Sayur(
+    val id_sayur: Int,
+    val id_petani: Int,
+    val nama_sayur: String,
+    val harga: Int,
+    val stok: Int,
+    val deskripsi: String,
+    val gambar: String? = null, // Nama file asli di database
+    val gambar_url: String? = null // URL lengkap dari server (http://10.0.2.2...)
+)
